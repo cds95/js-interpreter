50aeb5b8177b278c19249d8cab9cb4195edf19d2
cds95 <cds95@uw.edu>
2020-07-11 15:23:35 -0700

    Write eval for boolean, ints and symbols

--- app/JS/Core.hs
+++ app/JS/Core.hs
@@ -4,16 +4,22 @@ import Data.HashMap.Strict as H (HashMap, insert, lookup, empty, fromList)
 import Control.Monad.Except
 import Control.Monad.State
 
-type Env = H.HashMap String String 
+type Env = H.HashMap String Val
 data Val = Number Int |
            Boolean Bool |
+           JSString String |
            Nil | 
            Symbol String 
 
 instance Show Val where 
        show (Number a) = show a
+       show (Boolean a) = show a
 
-data Diagnostic = UnimplementedError String
-    deriving (Show)
+data Diagnostic = UnimplementedError String |
+                  UndefinedError String
+
+instance Show Diagnostic where 
+       show (UnimplementedError a) = a ++ " is unimplemented"
+       show (UndefinedError a) = a ++ " is undefined"
 
 type EvalState a = StateT Env (Except Diagnostic) a
--- app/JS/Eval.hs
+++ app/JS/Eval.hs
@@ -1,5 +1,16 @@
 module JS.Eval where 
 import JS.Core
 
+import Data.HashMap.Strict as H (HashMap, insert, lookup, empty, fromList)
+import Control.Monad.Except
+import Control.Monad.State
+
 eval :: Val -> EvalState Val
 eval v@(Number _) = return v
+eval v@(Boolean _) = return v 
+
+eval (Symbol s) = do 
+    env <- get 
+    case H.lookup s env of
+        Nothing -> throwError (UndefinedError s)
+        Just a -> return a
