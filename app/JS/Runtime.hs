88ab1a1e5ee65eb94653d0cd7657f9ccd7945d2d
cds95 <cds95@uw.edu>
2020-07-26 09:42:44 -0700

    Add comparators

--- app/JS/Runtime.hs
+++ app/JS/Runtime.hs
@@ -6,12 +6,33 @@ initialEnv :: Env
 initialEnv = H.fromList []
 
 runtimeOperations :: RuntimeOperationsEnv
-runtimeOperations = H.fromList [("+", liftPrimitiveAdd), ( "-", liftPrimitiveSubtract), ("/", liftPrimitiveDivide), ("*", liftPrimitiveMultiply), ("&&", liftPrimitiveAndBoolean), ("||", liftPrimitiveOrBoolean)]
+runtimeOperations = H.fromList [
+                        ("+", liftPrimitiveAdd), 
+                        ( "-", liftPrimitiveSubtract), 
+                        ("/", liftPrimitiveDivide), 
+                        ("*", liftPrimitiveMultiply), 
+                        ("&&", liftPrimitiveAndBoolean), 
+                        ("||", liftPrimitiveOrBoolean),
+                        (">", liftPrimitiveGreaterThan),
+                        ("<", liftPrimitiveLessThan),
+                        (">=", liftPrimitiveGreaterThanOrEqual),
+                        ("<=", liftPrimitiveLessThanOrEqual),
+                        ("!=", liftPrimitiveNotEqual),
+                        ("==", liftPrimitiveEqual)
+                    ]
 
 liftPrimitiveAdd (Num pOne) (Num pTwo) = Num (pOne + pTwo) 
 liftPrimitiveSubtract (Num pOne) (Num pTwo) = Num (pOne - pTwo)
 liftPrimitiveDivide (Num pOne) (Num pTwo) = Num (pOne `div` pTwo)
 liftPrimitiveMultiply (Num pOne) (Num pTwo) = Num (pOne * pTwo)
+liftPrimitiveGreaterThan (Num pOne) (Num pTwo) = Boolean (pOne > pTwo)
+liftPrimitiveLessThan (Num pOne) (Num pTwo) = Boolean (pOne < pTwo)
+liftPrimitiveGreaterThanOrEqual (Num pOne) (Num pTwo) = Boolean (pOne >= pTwo)
+liftPrimitiveLessThanOrEqual (Num pOne) (Num pTwo) = Boolean (pOne <= pTwo)
+liftPrimitiveNotEqual (Num pOne) (Num pTwo) = Boolean (pOne /= pTwo)
+liftPrimitiveNotEqual (Boolean a) (Boolean b) = Boolean (a /= b)
+liftPrimitiveEqual (Num pOne) (Num pTwo) = Boolean (pOne == pTwo)
+liftPrimitiveEqual (Boolean a) (Boolean b) = Boolean (a == b)
 
 liftPrimitiveAndBoolean (Boolean a) (Boolean b) = Boolean (a && b)
 liftPrimitiveOrBoolean (Boolean a) (Boolean b) = Boolean (a || b)
